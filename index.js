c78d858192dc111a44244de3aebfe35e3e6f2532
bettyje <bettyjelagat156@gmail.com>
2024-10-02 14:38:20 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 11;
+let num4 = 7;
+
+let mod = 11 % 7;
+
+let numbers = [5, 10, 20, 3, 15];
+
+let max = Math.max(...numbers);
+
